9057a076c2a1ce174db45be77716c6d805f4879a
jmhs11 <josemariahs@gmail.com>
2022-07-13 22:25:00 +0200

    Init layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-const App = () => {
-	return <div className='App'>Hello World</div>;
-};
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Error404 from "./pages/Error404";
+
+const App = () => (
+	<div id="layout" className="h-screen">
+		<header className="bg-blue-500">asdasd</header>
+		<aside className="bg-red-300">asdasdasdasdasdasdasd</aside>
+		<main className="bg-yellow-200">
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='*' element={<Error404 />} />
+			</Routes>
+		</main>
+	</div>
+);
 
 export default App;
