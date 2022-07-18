10f59fe688595e9b697f1b5b1ea0ea10ddcea957
jmhs11 <josemariahs@gmail.com>
2022-07-18 18:56:20 +0200

    Add Icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
+import {
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	BellIcon,
+	ChevronDownIcon,
+	MailIcon,
+	SearchIcon
+} from '@heroicons/react/outline';
+
 const Header = () => {
 	return (
-		<header className='fixed left-[20%] h-[10%] w-full bg-blue-500 px-4'>
-			<div className='flex items-center justify-between'>
+		<header className='fixed left-[20%] h-[10%] w-full bg-blue-500'>
+			<div className='flex items-center justify-between px-4'>
 				<div className='flex items-center gap-2'>
 					<div className='flex items-center h-full gap-1 w-fit'>
-						<p className='border rounded-md'>arrow-left</p>
-						<p className='border rounded-md'>arrow-right</p>
+						<ArrowLeftIcon className='icon-rounded' />
+						<ArrowRightIcon className='icon-rounded' />
 					</div>
 					<div className='flex items-center gap-2'>
-						<p className='border rounded-md'>searchIcon</p>
+						<SearchIcon className='icon' />
 						<input
 							type='search'
 							name='search'
@@ -24,12 +33,12 @@ const Header = () => {
 						<p>YOUR COINS</p>
 					</div>
 					<div className='flex items-center h-full gap-1 w-fit'>
-						<p className='border rounded-md'>MDs</p>
-						<p className='border rounded-md'>Notifications</p>
+						<MailIcon className='icon-rounded' />
+						<BellIcon className='icon-rounded' />
 					</div>
 				</div>
 			</div>
-			<div className='flex items-center border-l'>
+			<div className='flex items-center px-4 border-l'>
 				<div className='grid w-10 h-10 text-white bg-red-500 rounded-full aspect-square place-content-center'>
 					JH
 				</div>
@@ -37,7 +46,7 @@ const Header = () => {
 					<div>Nombre</div>
 					<div>Email</div>
 				</div>
-				<div>Arrow-down Icon</div>
+				<ChevronDownIcon className='icon' />
 			</div>
 		</header>
 	);
